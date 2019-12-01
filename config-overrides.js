b0b50be3d6510fb575ade42d4f6160881abc3605
Json-Code <571460635@qq.com>
2019-12-01 11:25:36 +0800

    配置less以及antd

--- config-overrides.js
+++ config-overrides.js
@@ -4,10 +4,18 @@
  */
 
 // 从customize-cra引入一些相关的方法
-const { override, addLessLoader } = require('customize-cra')
+const { override, addLessLoader,fixBabelImports } = require('customize-cra')
+
+const modifyVars = require('./lessVars')
 
 module.exports = override(
   addLessLoader({
-    javascriptEnabled: true
-  })
+    javascriptEnabled: true,
+    modifyVars
+  }),
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true, // `style: true` 会加载 less 文件
+  }),
 )
---
+++ lessVars.js
@@ -0,0 +1,26 @@
+module.exports = {
+  '@primary-color': 'yellow', // 全局主色
+  '@link-color': '#1890ff', // 链接色
+  '@success-color': '#52c41a', // 成功色
+  '@warning-color': '#faad14', // 警告色
+  '@error-color': '#f5222d', // 错误色
+  '@font-size-base': '14px', // 主字号
+  '@heading-color': 'rgba(0, 0, 0, 0.85)', // 标题色
+  '@text-color': 'rgba(0, 0, 0, 0.65)', // 主文本色
+  '@text-color-secondary ': 'rgba(0, 0, 0, .45)', // 次文本色
+  '@disabled-color ': 'rgba(0, 0, 0, .25)', // 失效色
+  '@border-radius-base': '4px', // 组件/浮层圆角
+  '@border-color-base': '#d9d9d9', // 边框色
+  '@box-shadow-base': '0 2px 8px rgba(0, 0, 0, 0.15)', // 浮层阴影
+}
+
+// 重命名变量: 选中变量名后按F2
+// 转到变量名的定义处: 选中变量名后按F12
+// 同时选择多个单词: Alt + Click
+// 同时选择上一行 (Ctrl + Alt + Up) 或者下一行 (Ctrl + Alt + Down) 的相同位置：
+// 依次找出文中所有的当前选中的单词: Ctrl + D
+// 一次性找出文所有的当前选中的单词: Ctrl + Shift + L
+// 拓展性 (Shift + Alt + Right) 或者收缩性 (Shift + Alt + Left) 的选中文本
+// 矩形框的鼠标选择: 同时按住Shit和Alt并使用鼠标进行拖拽选择
+// 切换编程语言语法: Ctrl + K M
+// 在上方复制一行Shift + Alt + Up，在下方复制一行Shift + Alt + Down
